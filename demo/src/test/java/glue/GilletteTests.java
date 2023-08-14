affb07f3152147e1c12ed5f8e432a072529f44c7
Iamneo <techadmin@iamneo.ai>
2023-08-14 13:52:53 +0000

    Backup Commit

--- demo/src/test/java/glue/GilletteTests.java
+++ demo/src/test/java/glue/GilletteTests.java
@@ -20,6 +20,7 @@ import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import cucumber.api.java.en.And;
 // import io.cucumber.java.After;
 // import io.cucumber.java.Before;
 // import io.cucumber.java.en.Given;
